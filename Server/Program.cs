d79077fb014e3ae6614c7bf9cf9ead2a88559c0e
Alex Holloway <alex.holloway.ctx@gmail.com>
2022-03-11 23:44:54 +0000

    Remove a thing

--- Server/Program.cs
+++ Server/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddMemoryCache();
 builder.Services.AddRazorPages();
 
 // TODO Change this to use PostgreSQL in the docker container
